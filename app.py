9e1ee8373c5c62315e5db5940db89016e4225884
JR2909 <j.r.k290905@gmail.com>
2025-05-17 21:52:51 +0200

    added darkmode minor updated

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def impressum():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    #app.run(debug=True)
+    app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-// Startet Autoplac auf index.html
+// Startet Autoplay auf index.html
 const videos = [
     "/static/videos/3264304-hd_1280_720_30fps.mp4",
     "/static/videos/3569293-uhd_3840_2160_24fps.mp4",
@@ -28,4 +28,25 @@ form.reset();
 }, 100);
 });
 });
+});
+
+// für Aktivierung des Darkmode
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+
+    // Prüfen, ob Dark Mode bereits aktiv ist
+    if (localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+    }
+
+    darkModeToggle.addEventListener("click", function () {
+        if (body.classList.contains("dark-mode")) {
+            body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        } else {
+            body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        }
+    });
 });
